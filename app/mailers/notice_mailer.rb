108a52fc9de722ebe2d6707f1dec5016f96006f9
hideyuki tabata <tahkyu3@outlook.jp>
2017-01-20 09:26:17 +0000

    sendmail

---
+++ app/mailers/notice_mailer.rb
@@ -0,0 +1,14 @@
+class NoticeMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notice_mailer.sendmail_blog.subject
+  #
+  def sendmail_blog(blog)
+    @blog = blog
+
+    mail to: "tahkyu3@outlook.jp",
+         subject: '【achive】ブログが投稿されました'
+  end
+end
