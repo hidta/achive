d9cf869212f232886b9212caceb4c10e4bd25eb8
hidta <tahkyu3@outlook.jp>
2017-02-22 03:14:29 +0000

    carreawave

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,3 @@
-# config valid only for current version of Capistrano
-lock '3.6.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
-server 'http://13.112.6.106/', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/home/app/.ssh/id_rsa'
+server '13.112.6.106', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/home/vagrant/.ssh/id_rsa'
